921ffd9135250e1039f9fb299886ec34c3fb7509
Bruno Pedrosa <bgpedrosas@gmail.com>
2020-10-14 15:22:45 -0300

    Hotfixes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -8,6 +8,12 @@ const appointmentsRepository = new AppointmentsRepository();
 
 const appointments: Appointment[] = [];
 
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+})
+
 appointmentsRouter.post("/", (req, res) => {
   const { provider, date } = req.body;
 
@@ -21,7 +27,10 @@ appointmentsRouter.post("/", (req, res) => {
     return res.status(400).json({ message: "Date already booked." });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate
+  });
 });
 
 export default appointmentsRouter;
