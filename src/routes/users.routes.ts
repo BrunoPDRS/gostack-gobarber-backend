9e63d5d6274e5be4baefb118dfe64d2237ff38e9
Bruno Pedrosa <bgpedrosas@gmail.com>
2020-12-30 13:24:00 -0300

    add image upload to users.routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,32 +1,40 @@
-import { Router } from "express";
-import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
-usersRouter.post("/", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+usersRouter.post('/', async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
 
-    const createUser = new CreateUserService();
+		const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+		const user = await createUser.execute({
+			name,
+			email,
+			password,
+		});
 
-    delete user.password;
+		delete user.password;
 
-    return res.json(user);
-  } catch (err) {
-    return res.status(400).json({ error: err.message });
-  }
+		return res.json(user);
+	} catch (err) {
+		return res.status(400).json({ error: err.message });
+	}
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (req, res) => {
-  return res.json({ ok: true });
-})
+usersRouter.patch(
+	'/avatar',
+	ensureAuthenticated,
+	upload.single('avatar'),
+	async (req, res) => {
+		return res.json({ ok: true });
+	}
+);
 
 export default usersRouter;
