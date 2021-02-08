89bbd935c3019155f519c963e9f4e0fd10d28eb3
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-07 21:19:44 -0300

    Rewriting Repositories

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,8 @@
 import Appointment from '../infra/typeorm/entities/Appointment'
 
-export default interface IAppointmentsRepository {
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO'
 
+export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>
 }
